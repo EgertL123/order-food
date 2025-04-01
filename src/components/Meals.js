30aba0c2457109c969da1d1ecb6d188723bd5386
Egert Linnas <egert.linnas@voco.ee>
2025-04-01 09:57:50 +0300

    Andmete pärimine Meals.js komponendis (Ül 3)

--- src/components/Meals.js
+++ src/components/Meals.js
@@ -1,11 +1,29 @@
+import { useEffect, useState } from "react";
+
 const Meals = () => {
-    return (
-        <ul id="meals">
-            { 
-              <li><strong>create list of meals, using fetch data from backend</strong></li>  // list of meals
+    const [meals, setMeals] = useState([])
+
+    useEffect(() => {
+        const fetchMeals = async () => {
+            try {
+                // Tuleb paring backendist
+                const response = await fetch("http://localhost:3001/meals") 
+                if (!response.ok) {
+                    throw new Error("Failed to fetch meals.")
+                }
+                const data = await response.json() 
+                console.log(data)
+                setMeals(data)
+            } catch (error) {
+                console.error("Error fetching meals:", error)
             }
-        </ul>
-    )
+        }
+
+        fetchMeals()
+    }, [])
+
+    return null
+
 }
 
-export default Meals
+export default Meals;
